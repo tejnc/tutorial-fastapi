a1804955ae61f149e63a7213fc26884318f4ef17
tejnc <tejnarayan15.tnc@gmail.com>
2022-05-21 15:01:56 +0545

    Added response model

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 
 from .database import engine, SessionLocal
 from . import schemas, models
@@ -55,13 +56,13 @@ def update(id, request: schemas.Blog ,db: Session = Depends(get_db)):
 
 
 
-@app.get("/blog")
+@app.get("/blog" , response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}", status_code=200)
+@app.get("/blog/{id}", status_code=200 , response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if  not blog:
