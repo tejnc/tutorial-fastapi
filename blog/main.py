c449dc39525ec91e5fbf8a703dba4e284ad1a5bc
tejnc <tejnarayan15.tnc@gmail.com>
2022-05-21 13:23:38 +0545

    simple crud operation implemented

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
 from .database import engine, SessionLocal
@@ -20,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -30,15 +30,33 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     # return {"title": request.title, "body": request.body}
 
 
+@app.delete("/blog/delete/{id}" ,status_code=status.HTTP_204_NO_CONTENT)
+def delete(id, db:Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {"data":"blog deleted"}
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog ,db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id)\
+        .update({"title":request.title, "body": request.body})
+    db.commit()
+    return "updated"
+
+
+
 @app.get("/blog")
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}")
-def show(id, db: Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if  not blog:
+        raise HTTPException(status_code=404, detail=f"Blog with the id {id} is not available.")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with the id {id} is not available."}
     return blog
-
-
