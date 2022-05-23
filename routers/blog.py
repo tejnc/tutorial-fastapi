35a29f5e0a28871f3bb1054fad0571ed0c7755be
tejnc <tejnarayan15.tnc@gmail.com>
2022-05-23 21:22:30 +0545

    simple name changes

--- routers/blog.py
+++ routers/blog.py
@@ -14,29 +14,29 @@ router = APIRouter(
 
 # get all the blogs
 @router.get("/" , response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def all(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.get_all(db)
 
 
 # create a blog post
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def create(request: schemas.Blog, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.create(request, db)
 
 
 # deleting the data based on id
 @router.delete("/delete/{id}" ,status_code=status.HTTP_204_NO_CONTENT)
-def delete(id, db:Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def delete(id, db:Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.delete(db ,id)
 
 
 # updating the data in the database
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog ,db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def update(id, request: schemas.Blog ,db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
    return blog.update(db,request,id)
 
 
 # get the blog with the specific id
 @router.get("/{id}", status_code=200 , response_model=schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def show(id, response: Response, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return blog.show(db,id)
--- routers/user.py
+++ routers/user.py
@@ -15,19 +15,19 @@ router = APIRouter(
 
 # Adding new user
 @router.post("/")
-def create_user(request: schemas.User , db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def create_user(request: schemas.User , db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return user.create_user(request,db)
 
 
 # Displaying users
 @router.get("/all", response_model=List[schemas.ShowUser])
-def show_users(db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def show_users(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     return user.show_user(db)
 
 
 # getting user id
 @router.get("/{id}",response_model=schemas.ShowUser)
-def get_user(id: int, db:Session=Depends(database.get_db), get_current_user: schemas.User = Depends(get_current_user)):
+def get_user(id: int, db:Session=Depends(database.get_db), current_user: schemas.User = Depends(get_current_user)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} is not available.")
